dfd1d343f761bce6fde03c1fb7e8c51fbc57833e
krthush <33790334+krthush@users.noreply.github.com>
2020-11-10 17:54:09 +0000

    Added bird enemy with basic A* path finding

---
+++ Assets/EnemyGFX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else if (aiPath.desiredVelocity.x <= 0.01)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
